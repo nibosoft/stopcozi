d5c22149ecfeba6cc97575db019d26be9c44ab9e
Nicolae Botez <nicolaebotez@yahoo.com>
2016-10-08 16:30:53 +0300

    Initial commit

---
+++ src/main/java/gov/ithub/StopcoziApplication.java
@@ -0,0 +1,12 @@
+package gov.ithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StopcoziApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StopcoziApplication.class, args);
+	}
+}
---
+++ src/test/java/gov/ithub/StopcoziApplicationTests.java
@@ -0,0 +1,16 @@
+package gov.ithub;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StopcoziApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
